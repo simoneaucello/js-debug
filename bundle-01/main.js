d4d2884f28007927d04ca0a258fad66cdda7bebb
simoneaucello <simoneaucello@gmail.com>
2024-03-08 15:37:05 +0100

    [add] bundle 1

--- bundle-01/main.js
+++ bundle-01/main.js
@@ -16,32 +16,56 @@ for (let i = 0; i > 5; i++) {
 }
 
 
+
 // ESERCIZIO 2
 function addIfEven(num) {
-    if (num % 2 = 0) {
-        return num + 5;
+    if (num % 2 === 0) {
+        // return num + 5;
     }
-    return num;
+    return num + 5;
 }
-
+console.log(addIfEven(5));
+// segno di uguaglianza sbagliato/ errore di sintassi 
+// return fuori dalla condizione if
 
 // ESERCIZIO 3
 function loopToFive() {
-    for (let i = 0, i < 5, i++) {
+    for (let i = 0; i < 5; i++) {
         console.log(i);
     }
 }
+// punto e virgola al posto della virgola/ errore di sintassi
 
 
 // ESERCIZIO 4 (suggerimento: ci sono 7 errori)
+// function displayEvenNumbers() {
+//     let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+//     let evenNumbers = [];
+//     for (let i = 0; i < numbers.length - 1; i++;) {
+//         if (numbers % 2 = 0); {
+//             evenNumbers.push(i);
+//         }
+//         return evenNumbers;
+//     }
+// }
+
 function displayEvenNumbers() {
     let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
     let evenNumbers = [];
-    for (let i = 0; i < numbers.length - 1; i++;) {
-        if (numbers % 2 = 0); {
-            evenNumbers.push(i);
+    for (let i = 0; i <= numbers.length -1; i++) {
+        if (numbers[i] % 2 === 0) {
+            evenNumbers.push(numbers[i]);
         }
-        return evenNumbers;
     }
+    return evenNumbers;
 }
 displayEvenNumbers(); // dovrebbe restituire [2,4,6,8]
+console.log(displayEvenNumbers());
+
+//nell'istruzione for il terzo parametro dovrebbe essere solo i++, senza il punto e virgola 
+// nell'if devo accedere all'elemento dell'array con numbers[i]
+// l'operatore = dovrebbe essere di confronto ===
+// return fuori dal ciclo for
+//pusho il valore dell'elemento corrente nell'array numbers
+//incremento i  solo quando trovo un numero pari
+// <= nel ciclo per includere l'ultimo elemento
